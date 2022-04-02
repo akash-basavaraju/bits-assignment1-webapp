48dcfdcae9c274f7c4c21acc4d87e6acf3df2cc2
Akash Basavaraju <bmnakash@gmail.com>
2022-04-02 14:35:58 +0530

    initial setup

---
+++ src/Pages/Main/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the Main Page in the Web App
+      </header>
+    </div>
+  );
+}
+
+export default App;
