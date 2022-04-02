53fa79d516b8b093244ee1947d53c44c946ab5c8
MaryBitGit <2021mt93505@wilp.bits-pilani.ac.in>
2022-04-02 15:44:02 +0530

    About Page addition

---
+++ src/Pages/About/About.js
@@ -0,0 +1,13 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="About">
+      <header className="About-header">
+        This is the About Page in the Web App
+      </header>
+    </div>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./Pages/Main/App";
+import About from "./Pages/About/About";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
