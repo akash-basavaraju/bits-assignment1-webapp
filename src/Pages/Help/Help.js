fbef9af4805bd3ee02630a7d2361e43b192215bb
Akash Basavaraju <bmnakash@gmail.com>
2022-04-03 14:31:02 +0530

    feat: help page addition

---
+++ src/Pages/Help/Help.js
@@ -0,0 +1,13 @@
+import "./Help.css";
+
+function Help() {
+  return (
+    <div className="Help">
+      <header className="Help-header">
+        This is the Help Page in the Web App
+      </header>
+    </div>
+  );
+}
+
+export default Help;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import Help from "./Pages/Help/Help";
 import App from "./Pages/Main/App";
 
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
