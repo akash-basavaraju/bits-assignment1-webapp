32b6524d8e65222d3bb2b99a5678c0aca94e258f
vilasvarghese <vilas.varghese@gmail.com>
2022-04-11 20:14:18 +0530

    added project

---
+++ src/Pages/Project/Project.js
@@ -0,0 +1,15 @@
+import Links from "../../Components/Links/Links";
+import "./Project.css";
+
+function Project() {
+  return (
+    <div className="Project">
+      <Links />
+      <header className="Project-header">
+        This is the Project Page in the Web App.
+      </header>
+    </div>
+  );
+}
+
+export default Project;
